20b6a3afc26b0e6a36f1dead882bcd3cbc6471e6
TimmyGOGO <temich941@mail.ru>
2018-12-27 11:24:32 +0300

    Go Typescript: Board (moving doesn't work and all is crashing even when refreshing)

---
+++ src/components/Board.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import Knight from './Knight';
+// import Square from './Square';
+// import { canMoveKnight, moveKnight } from './Game';
+// import { DragDropContextProvider } from 'react-dnd';
+// import HTML5Backend from 'react-dnd-html5-backend';
+import BoardSquare from './BoardSquare';
+
+export interface BoardProps {
+  knightPostition: [number, number]
+} 
+
+// for styled-components:
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+
+export default class Board extends React.Component<BoardProps> {
+  public render() {
+    const squares = [];
+    for (let i = 0; i < 64; i++) {
+      squares.push(this.renderSquare(i));
+    }
+
+    return <Container>{squares}</Container>;
+  }
+
+  private renderSquare(i: number) {
+    const x = i % 8;
+    const y = Math.floor(i / 8);
+
+    return (
+      <div key={i}
+           style={{ width: '12.5%', height: '12.5%' }}>
+        <BoardSquare x={x}
+                     y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
+      </div>
+    );
+  }
+
+  private renderPiece(x: number, y: number) {
+    const [knightX, knightY] = this.props.knightPostition;
+    const isKnightHere = x === knightX && y === knightY;
+    return isKnightHere ? <Knight /> : null;
+
+  }
+}
