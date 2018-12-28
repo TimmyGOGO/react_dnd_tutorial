70834f2d9d3d3b5deb25e74326c5f181ca364e75
TimmyGOGO <temich941@mail.ru>
2018-12-28 17:28:00 +0300

    Updates

---
+++ precache-manifest.78183dda898b82c36c96faad6eb21b2b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "39985573729aeb6a8d08",
+    "url": "/react_dnd_tutorial/static/js/runtime~main.39985573.js"
+  },
+  {
+    "revision": "dc0b7d614fcd7bfdbb37",
+    "url": "/react_dnd_tutorial/static/js/main.dc0b7d61.chunk.js"
+  },
+  {
+    "revision": "8e2d911ceb080a162eb6",
+    "url": "/react_dnd_tutorial/static/js/1.8e2d911c.chunk.js"
+  },
+  {
+    "revision": "eee2cf97294e0b96606ad12899ef740d",
+    "url": "/react_dnd_tutorial/index.html"
+  }
+];
